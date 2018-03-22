86343598c1e285ff0d28a38796f36e8d55b01e24
DomBro <460716809@qq.com>
2018-03-22 12:05:32 +0800

    MOD AMD CONT
    MOD:
    1.request.sql中定制不同方法的查询字段//request.sql
    
    AMD:
    1.增加request.sql中查询方法//request.sql
    
    CONT:
    1.MeetingService.java方法

--- src/main/webapp/WEB-INF/sql/request.sql
+++ src/main/webapp/WEB-INF/sql/request.sql
@@ -1,12 +1,20 @@
 #sql("findList")
-      SELECT r_id rid,name,m_id mid  FROM request
+      SELECT r_id rid,name,m_id mid FROM request
         #for(x:cond)
            #(for.index == 0 ? "where": "and") #(x.key) #para(x.value)
         #end
 #end
 
-#sql("find")
-      SELECT u_id uid, m_id mid,name,tel,remark,status  FROM request
+#sql("findByRid")
+      SELECT r_id rid,name,tel,remark FROM request
+        #for(x:cond)
+           #(for.index == 0 ? "where": "and") #(x.key) #para(x.value)
+        #end
+#end
+
+
+#sql("findListByUidAndStatus")
+      SELECT name,m_id mid, status FROM request
         #for(x:cond)
            #(for.index == 0 ? "where": "and") #(x.key) #para(x.value)
         #end
