031540deda69c38b057aad24eeba9bf77871a071
DomBro <460716809@qq.com>
2018-03-27 15:15:36 +0800

    PRBUG FIXBUG
    PRBUG:
    1.根据用户id获取对应群组失败，原因sql文件中字段之间忘了打逗号。
    
    FIXBUG:
    1.在 admin 和 conferee 加入逗号
    
    CONT:
    1.MeetingService.java方法

--- src/main/webapp/WEB-INF/sql/group.sql
+++ src/main/webapp/WEB-INF/sql/group.sql
@@ -1,5 +1,5 @@
 #sql("find")
-      SELECT g_id gid,m_id mid,admin conferee FROM meetmeeting.group
+      SELECT g_id gid,m_id mid,admin, conferee FROM meetmeeting.group
         #for(x:cond)
            #(for.index == 0 ? "where": "and") #(x.key) #para(x.value)
         #end
